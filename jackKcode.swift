f648847463356e54f71ea8e0934811cd83be2fd1
Jack Konrad Kooley <Jack@Jacks-MacBook-Pro-2.local>
2020-03-09 15:55:54 -0700

    comments

--- jackKcode.swift
+++ jackKcode.swift
@@ -1,3 +1,4 @@
+// top environment for program
 var top_env: [String:Value] = [
     "true" : BoolV(true),
     "false" : BoolV(false),
@@ -9,15 +10,16 @@ var top_env: [String:Value] = [
     "equal?" : PrimV(anyeq)
 ]
 
-
+// sum of two numVs, returns error if not numVs or wrong pattern
 func numPlus(values: [Value]) -> Value {
     switch values {
-    case numC:
+    case numV:
         
     }
     return 
 }
 
+// subtracts two numVs, returns error if not numVs or wrong pattern
 func numMinus(values: [Value]) -> Value {
     switch values {
     case numC:
@@ -26,6 +28,7 @@ func numMinus(values: [Value]) -> Value {
     return
 }
 
+// multiplys two numVs, returns error if not numVs or wrong pattern
 func numMult(values: [Value]) -> Value {
     switch values {
     case numC:
@@ -34,6 +37,7 @@ func numMult(values: [Value]) -> Value {
     return
 }
 
+// divides two numVs, returns error if not numVs or wrong pattern or second arg is 0
 func numDivide(values: [Value]) -> Value {
     switch values {
     case numC:
@@ -42,6 +46,7 @@ func numDivide(values: [Value]) -> Value {
     return
 }
 
+// checks if left numV is less than or equal to right numV, returns error if not numVs or more than 2
 func numleq(values: [Value]) -> Value {
     switch values {
     case numC:
@@ -50,6 +55,7 @@ func numleq(values: [Value]) -> Value {
     return
 }
 
+// checks if two values are the same value type and are equal to each other or not, returns error if wrong pattern
 func anyeq(values: [Value]) -> Value {
     switch values {
     case numC:
